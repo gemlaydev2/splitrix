c3df058cfa6ac5743e373f746028bc092fececdd
gemlay dev2 <gemalay.dev2@gemlay.com>
2025-05-29 15:09:04 +0530

    changes

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Stack, Text } from "tamagui";
+
+const login = () => {
+  return (
+    <Stack>
+      <Text>Login screen</Text>
+    </Stack>
+  );
+};
+
+export default login;
